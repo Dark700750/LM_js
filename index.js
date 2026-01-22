fa40a46246e3d92de242b181998619c1067e1513
Dev User <dev@local>
2026-01-22 15:20:16 +0545

    Change default port to 10000 for Render compatibility

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ connectDB();
 
 const app = express();
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 10000;
 //default middleware
 app.use(express.json());
 app.use(cookieParser());
